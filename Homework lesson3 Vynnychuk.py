c88bdd6d65f8201907250e538b56b882de050944
natali <nataliromanova010@gmail.com>
2021-06-18 16:21:38 +0300

    Homework lesson3 Vynnychuk.py

---
+++ Homework lesson3 Vynnychuk.py
@@ -0,0 +1,39 @@
+value = 50
+new_value = value // 2 if value < 100 else - value
+print(new_value)
+
+##################################################
+
+value = 50
+new_value = 1 if value < 100 else 0
+print(new_value)
+
+##################################################
+
+value = 150
+new_value = True if value < 100 else False
+print(new_value)
+
+##################################################
+
+my_str = "hillel"
+new_str = my_str.upper()
+print(new_str)
+
+##################################################
+
+my_str = "HILLEL"
+new_str = my_str.lower()
+print(new_str)
+
+#################################################
+
+my_str = "qwer"
+new_str = my_str * 2 if len(my_str) < 5 else my_str
+print(new_str)
+
+#################################################
+
+my_str = "qwer"
+new_str = my_str [::-1] if len(my_str) < 5 else my_str
+print(new_str)
--- Homework.py
+++ Homework.py
@@ -25,12 +25,36 @@
 # if value:
 #     print(value)
 
-value = 15
-if not value % 2 and not value % 3:
-    print("Число делится и на 2 и на 3 - т.е. на 6")
-elif not value % 2:
-    print("Число делится только на 2")
-elif not value % 3:
-    print("Число делится только на 3")
+# value = 15
+# if not value % 2 and not value % 3:
+#     print("Число делится и на 2 и на 3 - т.е. на 6")
+# elif not value % 2:
+#     print("Число делится только на 2")
+# elif not value % 3:
+#     print("Число делится только на 3")
+
+my_str="qwerty"
+# if 'r' in my_str:
+#     new_str = my_str * 2
+# else:
+#     new_str = my_str
+#
+new_str = my_str * 2 if 'r' in my_str else my_str
+print(new_str)
+
+# value = 50
+# new_value = value // 2 if value < 100 else - value
+# print(new_value)
+
+
+# my_int = 100
+# new_int = my_int // 2 if my_int > 10 else -1
+
+# print(new_int)
+
+
+my_str = "hillel"
+new_str = my_str.upper()
+print(new_str)
 
 
