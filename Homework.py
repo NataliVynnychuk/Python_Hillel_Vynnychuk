ac2fb85913e831c837730ed52f038f480c441907
natali <nataliromanova010@gmail.com>
2021-06-18 14:19:37 +0300

    add lesson.3.py homework.py

---
+++ Homework.py
@@ -0,0 +1,36 @@
+# www = 'www.conquer_and_command.net'
+# if ".com" in www:
+#     print("com in www")
+# else:
+#     print("com not in www")
+
+
+# value = "123456789"
+# my_str = value if len(value) <= 5 else value[2:5:2]
+# print(my_str[::-1])
+
+# value = 42
+
+# if value == 4:
+#     print("!!!!")
+#     print(value * 3)
+#     new_value = "asd" * value
+#     print(new_value)
+# else:
+#     print("else case")
+
+# my_bool = False
+# value = str(my_bool)
+#
+# if value:
+#     print(value)
+
+value = 15
+if not value % 2 and not value % 3:
+    print("Число делится и на 2 и на 3 - т.е. на 6")
+elif not value % 2:
+    print("Число делится только на 2")
+elif not value % 3:
+    print("Число делится только на 3")
+
+
