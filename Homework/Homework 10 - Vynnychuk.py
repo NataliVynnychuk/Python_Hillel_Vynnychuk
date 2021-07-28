28502b51788c1fe7f7e6ef3fd1bf4c719bc5e2e6
natali <nataliromanova010@gmail.com>
2021-07-28 16:47:13 +0300

    Homework 10 - Vynnychuk.py

---
+++ Homework/Homework 10 - Vynnychuk.py
@@ -0,0 +1,85 @@
+# Все пункты сделать как отдельные функции(можно создавать дополнительные вспомагательные функции)
+import os
+import string
+
+def get_data(filename):
+    with open(filename, "r") as txt_file:
+        data = txt_file.readlines()
+        return data
+
+# 1. Написать функцию, которая получает в виде параметра имя файла названия интернет доменов (domains.txt)
+#  и возвращает их в виде списка строк (названия возвращать без точки).
+
+def get_domains(filename):
+    list_domains = [value.replace(".", "").replace("\n", "") for value in get_data("domains.txt")]
+    return list_domains
+
+print(get_domains("domains.txt"))
+
+# 2. Написать функцию, которая получает в виде параметра имя файла (names.txt)
+# и возвращает список всех фамилий из него.
+# Каждая строка файла содержит номер, фамилию, страну, некоторое число (таблица взята с википедии).
+# Разделитель - символ табуляции "\t"
+
+def get_names(filename):
+    list_names = [value.split("\t")[1] for value in get_data("names.txt")]
+    return list_names
+
+print(get_names("names.txt"))
+
+# 3. Написать функцию, которая получает в виде параметра имя файла (authors.txt) и возвращает список
+# словарей вида {"date_original": date_original, "date_modified": date_modified}
+# в которых date_original - это дата из строки (если есть),
+# а date_modified - эта же дата, представленная в формате "dd/mm/yyyy" (d-день, m-месяц, y-год)
+# Например [{"date_original": "8th February 1828", "date_modified": 08/02/1828},  ...]
+
+months_dict = {"January" : "01",
+               "February": "02",
+               "March": "03",
+               "April": "04",
+               "May": "05",
+               "June": "06",
+               "July": "07",
+               "August": "08",
+               "September": "09",
+               "October": "10",
+               "November": "11",
+               "December": "12"}
+
+
+def get_list_date(filename):
+    original_date = [date.split(" -")[0] for date in get_data(filename) if date[0].isdigit() and date.split(" -")[0][-1].isdigit()]
+    return original_date
+
+
+def get_day(filename):
+    list_day = [date.split(" ")[0].rstrip("nsthrd") for date in get_list_date(filename)]
+    day_modified_list = []
+    for day in list_day:
+        if len(day) == 1:
+            day_modified_list.append("0" + day)
+        else:
+            day_modified_list.append(day)
+    return day_modified_list
+
+
+def get_months(filename):
+    list_months = [date.split(" ")[1] for date in get_list_date(filename)]
+    months_modified_list = [months_dict[months] for months in list_months]
+    return months_modified_list
+
+
+def get_years(filename):
+    list_years = [date.split(" ")[2] for date in get_list_date(filename)]
+    return list_years
+
+
+def get_dicts(filename):
+    date_original = [{"date_original": date} for date in get_list_date(filename)]
+    date_modified = ["/".join(data) for data in list(zip(get_day(filename), get_months(filename), get_years(filename)))]
+    for index, date in enumerate(date_original):
+        date["date_modified"] = date_modified[index]
+    return date_original
+
+print(get_dicts("authors.txt"))
+
--- Lesson/Lesson 10 - 21.07.py
+++ Lesson/Lesson 10 - 21.07.py
@@ -1,46 +1,73 @@
 # 10
 import os
 
-# current_dir = os.getcwd()
-# print(current_dir)
+current_dir = os.getcwd()
+print(current_dir)
 
-# base_dir = "../Homework"
-# def get_files_from_dir(base_dir, full_path=True):
-# list_dir = os.listdir(base_dir)
-# print(list_dir)
-# files = []
-# for file_object in list_dir:
-#     path_object = os.path.join(base_dir, file_object)
-#     if os.path.isfile(path_object):
-#         files.append(path_object if full_path else file_object)
-#     return files
-# homeworks_files = get_files_from_dir("Homework", full_path = False)
-# print(homeworks_files)
+list_dir = os.listdir("../Homework")
+print(list_dir)
 
-# def create_dir(path):
-#     try:
-#         os.mkdir("test_dir")
-#     except FileExistsError:
-#         pass
 
-# os.makedirs("test_3_dir/test_4_dir", exist_ok=True)
+base_dir = "../Homework"
+def get_files_from_dir(base_dir, full_path=True):
+list_dir = os.listdir(base_dir)
+print(list_dir)
+files = []
+for file_object in list_dir:
+    path_object = os.path.join(base_dir, file_object)
+    if os.path.isfile(path_object):
+        files.append(path_object if full_path else file_object)
+    return files
+homeworks_files = get_files_from_dir("Homework", full_path = False)
+print(homeworks_files)
+
+def create_dir(path):
+    try:
+        os.mkdir("test_dir")
+    except FileExistsError:
+        pass
+
+os.makedirs("test_3_dir/test_4_dir", exist_ok=True)
 
 ####################################################################
-# with open("Lesson 10 - 21.07.py", "r" ) as py_file:
-#     data = py_file.readlines()
-# print(data, type(data))
-#
-# data.append("# FINISH")
-# with open("Lesson 10_new - 21.07.py", "w" ) as py_file:
-#     py_file.writelines(data)
-#
-# for line in data:
-#     if len(line) > 32:
-#         print(line.split("\t"))
+with open("Lesson 10 - 21.07.py", "r" ) as py_file:
+    data = py_file.readlines()
+print(data, type(data))
+
+data.append("# FINISH")
+with open("Lesson 10_new - 21.07.py", "w" ) as py_file:
+    py_file.writelines(data)
+
+for line in data:
+    if len(line) > 32:
+        print(line.split("\t"))
 
 ###################################################################
 def create_dir(path):
-    pass
+    os.makedirs(path, exist_ok=True)
+
+
+def create_file(path, symbol):
+    filename = f"{path}/{symbol}.txt"
+    with open(filename, 'w') as txt_file:
+        txt_file.write(string.ascii_lowercase.replace(symbol, symbol.upper()))
+
+
+def create_files_in_dir(path):
+    for letter in string.ascii_lowercase:
+        create_file(path, letter)
+
+
+def get_tanos_click(path):
+    files = os.listdir(path)
+    files = list(set(files))[:len(files) // 2]
+    for file in files:
+        os.remove(os.path.join(path, file))
+
+
+# create_dir("alphabet")
+create_files_in_dir("alphabet")
+get_tanos_click("alphabet")
 
 
 
